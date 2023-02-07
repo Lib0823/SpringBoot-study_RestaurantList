d3c4df30bfc7e56be9298020432be060c8f6d59d
Lib0823 <robot082373@naver.com>
2023-02-07 23:23:12 +0900

    feat : Memory DB 테스트 코드 작성

--- src/main/java/com/example/restaurant/db/MemoryDbRepositoryAbstract.java
+++ src/main/java/com/example/restaurant/db/MemoryDbRepositoryAbstract.java
@@ -17,16 +17,37 @@ abstract public class MemoryDbRepositoryAbstract<T extends MemoryDbEntity> imple
 
     @Override
     public T save(T entity) {
-        return null;
+
+        var optionalEntity = db.stream().filter(it -> it.getIndex() == entity.getIndex()).findFirst();
+
+        if(optionalEntity.isEmpty()){
+            // db 에 데이터가 없는 경우
+            index++;
+            entity.setIndex(index);
+            db.add(entity);
+            return entity;
+        }else{
+            // db 에 이미 데이터가 있는 경우
+            var preIndex = optionalEntity.get().getIndex();
+            entity.setIndex(preIndex);
+
+            deleteById(preIndex);
+            db.add(entity);
+            return entity;
+        }
     }
 
     @Override
     public void deleteById(int index) {
+        var optionalEntity = db.stream().filter(it -> it.getIndex() == index).findFirst();
+        if(optionalEntity.isPresent()){ // 데이터가 있다면
+            db.remove(optionalEntity.get());
+        }
 
     }
 
     @Override
     public List<T> listAll() {
-        return null;
+        return db;
     }
 }
---
+++ src/test/java/com/example/restaurant/wishlist/repository/WishListRepositoryTest.java
@@ -0,0 +1,61 @@
+package com.example.restaurant.wishlist.repository;
+
+import com.example.restaurant.wishlist.entity.WishListEntity;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class WishListRepositoryTest {
+
+    @Autowired
+    private WishListRepository wishListRepository;
+
+    private WishListEntity create() {
+        var wishList = new WishListEntity();
+        wishList.setTitle("title");
+        wishList.setCategory("category");
+        wishList.setAddress("address");
+        wishList.setReadAddress("readAddress");
+        wishList.setHomePageLink("");
+        wishList.setImageLink("");
+        wishList.setVisit(false);
+        wishList.setVisitCount(0);
+        wishList.setLastVisitDate(null);
+
+        return wishList;
+    }
+
+    @Test
+    public void saveTest(){
+        var wishListEnity = create();
+        var expected = wishListRepository.save(wishListEnity);
+
+        Assertions.assertNotNull(expected);
+        Assertions.assertEquals(1, expected.getIndex());
+
+    }
+
+    @Test
+    public void findByIdTest(){
+        var wishListEnity = create();
+        wishListRepository.save(wishListEnity);
+
+        var expected = wishListRepository.findById(1);
+
+        Assertions.assertEquals(true, expected.isPresent());
+        Assertions.assertEquals(1, expected.get().getIndex());
+    }
+
+    @Test
+    public void deleteTest() {
+
+    }
+
+    @Test
+    public void listAllTest() {
+
+    }
+
+}
